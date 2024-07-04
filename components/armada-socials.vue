43a068c015ada4c7f85d1b684e027efec12816b2
The Enthusiast <whitedronetech@gmail.com>
2024-07-04 21:23:55 +0100

    chore: added seo meta on server side

--- components/armada-socials.vue
+++ components/armada-socials.vue
@@ -1,20 +1,21 @@
 <template>
     <div class="flex space-x-8 lg:space-x-16">
-        <a href="https://x.com/armadazone" target="_blank" rel="noopener noreferrer"
-            class="text-gray-500 hover:text-blue-500">
-            <img src="~/assets/socials/x.svg" class="lg:w-8 w-6 lg:h-8 h-6" />
+        <a href="https://x.com/armadazone" target="_blank" rel="noopener noreferrer" class="text-gray-500 ">
+            <img src="https://img.icons8.com/?size=100&id=A4DsujzAX4rw&format=png&color=ffffff"
+                class="lg:w-8 w-6 lg:h-8 h-6" />
         </a>
         <a href="https://www.instagram.com/armadazonehq" target="_blank" rel="noopener noreferrer"
-            class="text-gray-500 hover:text-pink-500">
-            <img src="~/assets/socials/instagram.svg" class="lg:w-8 w-6 lg:h-8 h-6" />
+            class="text-gray-500 ">
+            <img src="https://img.icons8.com/?size=100&id=32309&format=png&color=ffffff"
+                class="lg:w-8 w-6 lg:h-8 h-6" />
         </a>
-        <a href="https://www.tiktok.com/@armadazone" target="_blank" rel="noopener noreferrer"
-            class="text-gray-500 hover:text-black">
-            <img src="~/assets/socials/tiktok.svg" class="lg:w-8 w-6 lg:h-8 h-6" />
+        <a href="https://www.tiktok.com/@armadazone" target="_blank" rel="noopener noreferrer" class="text-gray-500 ">
+            <img src="https://img.icons8.com/?size=100&id=118638&format=png&color=ffffff"
+                class="lg:w-8 w-6 lg:h-8 h-6" />
         </a>
-        <a href="https://wa.me/+2348184510124" target="_blank" rel="noopener noreferrer"
-            class="text-gray-500 hover:text-green-500">
-            <img src="~/assets/socials/whatsapp.svg" class="lg:w-8 w-6 lg:h-8 h-6" />
+        <a href="https://wa.me/+2348184510124" target="_blank" rel="noopener noreferrer" class="text-gray-500 ">
+            <img src="https://img.icons8.com/?size=100&id=16733&format=png&color=ffffff"
+                class="lg:w-8 w-6 lg:h-8 h-6 object-contain" />
         </a>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,47 +2,40 @@
   <armada-hero />
 </template>
 <script setup lang="ts">
+const title = 'Buy New, Used, and Accidented Cars | Car Rentals & Savings Plans - Armada';
+const siteName = 'Armada';
+const siteImage = 'https://armadazone.com/images/luxury.png'
+const description = 'Discover a wide selection of new, used, and accidented cars at Armada. We also offer car rentals and innovative savings plans to help you buy your dream car. Explore exotic and regular cars, save towards your purchase, and enjoy an immersive car-buying experience. Visit our site for the latest inventory updates and expert car tips.';
   useHead({
-    title: 'Buy New, Used, and Accidented Cars | Car Rentals & Savings Plans - Armada',
+    title,
     meta: [
-      { name: 'description', content: 'Discover a wide selection of new, used, and accidented cars at Armada. We also offer car rentals and innovative savings plans to help you buy your dream car. Explore exotic and regular cars, save towards your purchase, and enjoy an immersive car-buying experience. Visit our site for the latest inventory updates and expert car tips.' },
+      { name: 'description', content: description },
       { name: 'keywords', content: 'buy cars, new cars, used cars, accidented cars, car rentals, car savings plans, exotic cars, regular cars, car dealership, car finance, automotive sales, luxury cars, affordable cars, car buying tips' },
       { name: 'robots', content: 'index, follow' },
-      { name: 'author', content: 'Armada' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
-      { property: 'og:title', content: 'Buy New, Used, and Accidented Cars | Car Rentals & Savings Plans - Armada' },
-      { property: 'og:description', content: 'Discover a wide selection of new, used, and accidented cars at Armada. We also offer car rentals and innovative savings plans to help you buy your dream car. Explore exotic and regular cars, save towards your purchase, and enjoy an immersive car-buying experience. Visit our site for the latest inventory updates and expert car tips.' },
-      { property: 'og:url', content: 'https://armadazone.com' },
-      { property: 'og:type', content: 'website' },
-      { property: 'og:image', content: 'https://armadazone.com/images/luxury.png' },
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:title', content: 'Buy New, Used, and Accidented Cars | Car Rentals & Savings Plans - Armada' },
-      { name: 'twitter:description', content: 'Discover a wide selection of new, used, and accidented cars at Armada. We also offer car rentals and innovative savings plans to help you buy your dream car. Explore exotic and regular cars, save towards your purchase, and enjoy an immersive car-buying experience. Visit our site for the latest inventory updates and expert car tips.' },
-      { name: 'twitter:image', content: 'https://armadazone.com/images/luxury.png' },
-      { name: 'twitter:site', content: '@armadazone' },
-      { name: 'instagram:card', content: 'summary_large_image' },
-      { name: 'instagram:title', content: 'Buy New, Used, and Accidented Cars | Car Rentals & Savings Plans - Armada' },
-      { name: 'instagram:description', content: 'Discover a wide selection of new, used, and accidented cars at Armada. We also offer car rentals and innovative savings plans to help you buy your dream car. Explore exotic and regular cars, save towards your purchase, and enjoy an immersive car-buying experience. Visit our site for the latest inventory updates and expert car tips.' },
-      { name: 'instagram:image', content: 'https://armadazone.com/images/luxury.png' },
-      { name: 'instagram:site', content: '@armadazonehq' },
-      { name: 'tiktok:card', content: 'summary_large_image' },
-      { name: 'tiktok:title', content: 'Buy New, Used, and Accidented Cars | Car Rentals & Savings Plans - Armada' },
-      { name: 'tiktok:description', content: 'Discover a wide selection of new, used, and accidented cars at Armada. We also offer car rentals and innovative savings plans to help you buy your dream car. Explore exotic and regular cars, save towards your purchase, and enjoy an immersive car-buying experience. Visit our site for the latest inventory updates and expert car tips.' },
-      { name: 'tiktok:image', content: 'https://armadazone.com/images/luxury.png' },
-      { name: 'tiktok:site', content: '@armadazone' },
-      { name: 'whatsapp:card', content: 'summary_large_image' },
-      { name: 'whatsapp:title', content: 'Buy New, Used, and Accidented Cars | Car Rentals & Savings Plans - Armada' },
-      { name: 'whatsapp:description', content: 'Discover a wide selection of new, used, and accidented cars at Armada. We also offer car rentals and innovative savings plans to help you buy your dream car. Explore exotic and regular cars, save towards your purchase, and enjoy an immersive car-buying experience. Visit our site for the latest inventory updates and expert car tips.' },
-      { name: 'whatsapp:image', content: 'https://armadazone.com/images/luxury.png' },
-    ]
+      { name: 'author', content: siteName },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }
+  ]
 })
 
 useSeoMeta({
-  title: 'Buy New, Used, and Accidented Cars | Car Rentals & Savings Plans - Armada',
-  ogTitle: 'Buy New, Used, and Accidented Cars | Car Rentals & Savings Plans - Armada',
-  description: 'Discover a wide selection of new, used, and accidented cars at Armada. We also offer car rentals and innovative savings plans to help you buy your dream car. Explore exotic and regular cars, save towards your purchase, and enjoy an immersive car-buying experience. Visit our site for the latest inventory updates and expert car tips.',
-  ogDescription: 'Discover a wide selection of new, used, and accidented cars at Armada. We also offer car rentals and innovative savings plans to help you buy your dream car. Explore exotic and regular cars, save towards your purchase, and enjoy an immersive car-buying experience. Visit our site for the latest inventory updates and expert car tips.',
-  ogImage: 'https://armadazone.com/images/luxury.png',
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description,
+  ogImage: siteImage,
   twitterCard: 'summary_large_image',
+})
+
+useServerSeoMeta({
+  title,
+  ogTitle: title,
+  description: description,
+  ogDescription: description,
+  ogImage: siteImage,
+  twitterCard: 'summary_large_image',
+  ogImageUrl: siteImage,
+  twitterTitle: title,
+  twitterDescription: description,
+  twitterImage: siteImage
 })
 </script>
