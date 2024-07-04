d867e4ab87c0ad5d01cb78f07194c9df5fa81c3a
The Enthusiast <whitedronetech@gmail.com>
2024-07-04 20:54:20 +0100

    chore: made mobile socials more responsive

--- components/armada-socials.vue
+++ components/armada-socials.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="flex space-x-16">
+    <div class="flex space-x-8 lg:space-x-16">
         <a href="https://x.com/armadazone" target="_blank" rel="noopener noreferrer"
             class="text-gray-500 hover:text-blue-500">
-            <img src="~/assets/socials/x.svg" class="w-8 h-8" />
+            <img src="~/assets/socials/x.svg" class="lg:w-8 w-6 lg:h-8 h-6" />
         </a>
         <a href="https://www.instagram.com/armadazonehq" target="_blank" rel="noopener noreferrer"
             class="text-gray-500 hover:text-pink-500">
-            <img src="~/assets/socials/instagram.svg" class="w-8 h-8" />
+            <img src="~/assets/socials/instagram.svg" class="lg:w-8 w-6 lg:h-8 h-6" />
         </a>
         <a href="https://www.tiktok.com/@armadazone" target="_blank" rel="noopener noreferrer"
             class="text-gray-500 hover:text-black">
-            <img src="~/assets/socials/tiktok.svg" class="w-8 h-8" />
+            <img src="~/assets/socials/tiktok.svg" class="lg:w-8 w-6 lg:h-8 h-6" />
         </a>
         <a href="https://wa.me/+2348184510124" target="_blank" rel="noopener noreferrer"
             class="text-gray-500 hover:text-green-500">
-            <img src="~/assets/socials/whatsapp.svg" class="w-8 h-8" />
+            <img src="~/assets/socials/whatsapp.svg" class="lg:w-8 w-6 lg:h-8 h-6" />
         </a>
     </div>
 </template>
